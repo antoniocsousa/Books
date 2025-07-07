22a25895dda6d2f3e059c0ed8882c28f25ba70ee
Antônio Sousa <antoniocsousa89@gmail.com>
2025-07-07 19:02:55 -0300

    configuração básica do react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>
+          
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
